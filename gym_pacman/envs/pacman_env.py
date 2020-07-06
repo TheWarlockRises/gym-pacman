2851ef9e36af6d26e3e684f9c3f63cac60c2fb2c
Cappycot <chriswang444@gmail.com>
2020-07-06 16:37:52 -0500

    Custom env skeleton code

---
+++ gym_pacman/envs/pacman_env.py
@@ -0,0 +1,20 @@
+import gym
+
+
+class PacmanEnv(gym.Env):
+    metadata = {"render.modes": ["default"]}
+
+    def __init__(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self, mode="default"):
+        pass
+
+    def close(self):
+        pass

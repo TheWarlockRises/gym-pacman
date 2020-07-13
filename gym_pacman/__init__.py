6795667b3c4299325f05fe78f3b1096c0517433c
Cappycot <chriswang444@gmail.com>
2020-07-13 10:32:27 -0500

    Add folders to setup

--- gym_pacman/__init__.py
+++ gym_pacman/__init__.py
@@ -1,8 +1,5 @@
 from gym.envs.registration import register
 
-from .envs import PacmanEnv
-from .res import *
-
 register(
     id='Pacman-v0',
     entry_point='gym_pacman.envs:PacmanEnv',
--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(name="gym_pacman",
       version="0.1.0",
       install_requires=["gym", "pygame"],
-      packages=["gym_pacman"]
+      packages=["gym_pacman", "gym_pacman.envs", "gym_pacman.res"]
 )

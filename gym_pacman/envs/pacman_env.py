7bfc7449e7553e338727aa2d6bb6c2fbddaed883
Cappycot <chriswang444@gmail.com>
2020-07-13 09:41:56 -0500

    Add gym imports

--- gym_pacman/envs/pacman_env.py
+++ gym_pacman/envs/pacman_env.py
@@ -1,19 +1,12 @@
 import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
 
 from ..fruit import *
 from ..game import *
 from ..ghost import *
 from ..level import *
 from ..path_finder import *
-
-# Joystick defaults - maybe add a Preferences dialog in the future?
-JS_DEVNUM = 0  # device 0 (pygame joysticks always start at 0). if JS_DEVNUM is not a valid device, will use 0
-JS_XAXIS = 0  # axis 0 for left/right (default for most joysticks)
-JS_YAXIS = 1  # axis 1 for up/down (default for most joysticks)
-JS_STARTBUTTON = 0  # button number to start the game. this is a matter of personal preference, and will vary from
-
-
-# device to device
 
 
 # game "mode" variable
@@ -31,7 +24,7 @@ JS_STARTBUTTON = 0  # button number to start the game. this is a matter of perso
 # TODO: Skip over pause frames (e.g. pause when ghost eaten)
 
 class PacmanEnv(gym.Env):
-    metadata = {"render.modes": ["default"]}
+    metadata = {"render.modes": ["human"]}
 
     def __init__(self, gui=False, pauses=False, sound=False):
         self.gui = gui
@@ -77,64 +70,6 @@ class PacmanEnv(gym.Env):
         if gui:
             pygame.display.set_mode(self.thisGame.screenSize)
             self.screen = pygame.display.get_surface()
-
-    """def init_pygame(self):
-        # initialise the joystick
-        if pygame.joystick.get_count() > 0:
-            if JS_DEVNUM < pygame.joystick.get_count():
-                js = pygame.joystick.Joystick(JS_DEVNUM)
-            else:
-                js = pygame.joystick.Joystick(0)
-            js.init()
-        else:
-            js = None"""
-
-    """def CheckInputs():
-        if thisGame.mode == 1 or thisGame.mode == 8 or thisGame.mode == 9:
-            if pygame.key.get_pressed()[pygame.K_RIGHT] or (
-                    js is not None and js.get_axis(JS_XAXIS) > 0.5):
-                if not (
-                        player.velX == player.speed and player.velY == 0) and not thisLevel.CheckIfHitWall(
-                        (player.x + player.speed, player.y),
-                        (player.nearestRow, player.nearestCol)):
-                    player.velX = player.speed
-                    player.velY = 0
-
-            elif pygame.key.get_pressed()[pygame.K_LEFT] or (
-                    js is not None and js.get_axis(JS_XAXIS) < -0.5):
-                if not (
-                        player.velX == -player.speed and player.velY == 0) and not thisLevel.CheckIfHitWall(
-                        (player.x - player.speed, player.y),
-                        (player.nearestRow, player.nearestCol)):
-                    player.velX = -player.speed
-                    player.velY = 0
-
-            elif pygame.key.get_pressed()[pygame.K_DOWN] or (
-                    js is not None and js.get_axis(JS_YAXIS) > 0.5):
-                if not (
-                        player.velX == 0 and player.velY == player.speed) and not thisLevel.CheckIfHitWall(
-                        (player.x, player.y + player.speed),
-                        (player.nearestRow, player.nearestCol)):
-                    player.velX = 0
-                    player.velY = player.speed
-
-            elif pygame.key.get_pressed()[pygame.K_UP] or (
-                    js is not None and js.get_axis(JS_YAXIS) < -0.5):
-                if not (
-                        player.velX == 0 and player.velY == -player.speed) and not thisLevel.CheckIfHitWall(
-                        (player.x, player.y - player.speed),
-                        (player.nearestRow, player.nearestCol)):
-                    player.velX = 0
-                    player.velY = -player.speed
-
-        if pygame.key.get_pressed()[pygame.K_ESCAPE] or (
-                js is not None and js.get_button(7)):
-            sys.exit(0)
-
-        elif thisGame.mode == 3:
-            if pygame.key.get_pressed()[pygame.K_RETURN] or (
-                    js is not None and js.get_button(JS_STARTBUTTON)):
-                thisGame.StartNewGame()"""
 
     def check_inputs(self, action):
         if action == 0 and not self.thisLevel.CheckIfHitWall(
@@ -343,7 +278,7 @@ class PacmanEnv(gym.Env):
         # the static method CheckIfHitSomething() in lines 108-111 of level.py regards obtaining all pellets
         pass
 
-    def render(self, mode="default"):
+    def render(self, mode="human"):
         if not self.gui:
             return
         global rect_list

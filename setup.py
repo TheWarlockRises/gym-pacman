8c6ef7005192643b6ab79c72c528fa1555c2fa14
Cappycot <chriswang444@gmail.com>
2020-07-13 15:04:48 -0500

    Include package data

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(name="gym_pacman",
       version="0.1.0",
       install_requires=["gym", "pygame"],
-      packages=["gym_pacman", "gym_pacman.envs", "gym_pacman.res"]
+      packages=["gym_pacman", "gym_pacman.envs"],
+      include_package_data=True
 )

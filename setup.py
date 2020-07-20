8fdd4b4fc63f3e6e6e3dcc24b4dfa52d0df9cf61
Cappycot <chriswang444@gmail.com>
2020-07-20 09:37:21 -0500

    Add numpy to requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name="gym_pacman",
       version="0.1.0",
-      install_requires=["gym", "pygame"],
+      install_requires=["gym", "numpy", "pygame"],
       packages=["gym_pacman", "gym_pacman.envs"],
       include_package_data=True
 )

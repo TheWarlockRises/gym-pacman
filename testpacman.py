ea9b62c7fb924a88df76cff27c19e4f9a6e86214
Cappycot <chriswang444@gmail.com>
2020-08-05 11:43:39 -0500

    Add test params

--- testpacman.py
+++ testpacman.py
@@ -12,11 +12,18 @@ channel_backgound = pygame.mixer.Channel(6)
 clock = pygame.time.Clock()
 pygame.init()
 
+offset = True
+pausing = False
+sensor_range = 10
+width = 2
+
 pacman = PacmanEnv(invincible=False, randomized=True,
-                   sensor=sensor_1d_4(10, offset=True, width=2))
+                   sensor=sensor_1d_4(sensor_range, offset=offset, width=width))
 pacman.reset()
 
 direc = 0
+offset = 0 if offset else 1
+w = width * 2 + 1
 
 while True:
     # Keep window from complaining unresponsive
@@ -25,7 +32,7 @@ while True:
             exit(0)
     pacman.render()
     observation, _, done, info = pacman.step(direc)
-    pause = False
+    pause = pausing
     if pause:
         print(observation)
     while pause:
@@ -35,7 +42,15 @@ while True:
         clock.tick(60)
     else:
         clock.tick(40)
-    if random.random() < 0.05 or info["pacmanx"] == 0 and info["pacmany"] == 0:
+    if random.random() < 0.05:
         direc = random.randint(0, 3)
+    elif info["pacmanx"] == 0 and info["pacmany"] == 0:
+        direc = random.randint(0, 3)
+        possible = []
+        for i in range(4):
+            if observation[offset][i * w + width] > 0:
+                possible.append(i)
+        if len(possible) > 0:
+            direc = possible[random.randint(0, len(possible) - 1)]
     if done:
         pacman.reset()
